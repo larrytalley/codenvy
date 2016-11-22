1c3dfc3d83bb7b9b624b0e068d1b2d6a7e006bba
larrytalley <larrytalley@users.noreply.github.com>
2016-11-22 12:22:11 -0900

    with some docs

--- webserver-webrick.rb
+++ webserver-webrick.rb
@@ -1,7 +1,9 @@
 # Thanks for the template to Integralist at
 # https://gist.github.com/Integralist/2862917
+# See also https://ruby-doc.org/stdlib-2.0.0/libdoc/webrick/rdoc/WEBrick.html
 require "webrick"
 
+# Provide a subclass of AbstractServlet to handle requests and craft responses
 class MyServlet < WEBrick::HTTPServlet::AbstractServlet
     def do_GET (request, response)
       STDERR.puts request
@@ -11,9 +13,13 @@ class MyServlet < WEBrick::HTTPServlet::AbstractServlet
     end
 end
 
+# Connect the server with a port
 webserver = WEBrick::HTTPServer.new(:Port => 3000)
+# Provide a way to quit
 trap("INT") {
   webserver.shutdown
 }
+# Mount our subclass at /
 webserver.mount "/", MyServlet
+# Start our server
 webserver.start
