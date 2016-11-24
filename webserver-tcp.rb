434feba298fc4f1f607c036a05e25bdb51e7ab0a
Larry Talley <larryt@acm.org>
2016-11-24 06:36:06 +0000

    works

--- webserver-tcp.rb
+++ webserver-tcp.rb
@@ -2,7 +2,7 @@
 # http://practicingruby.com/articles/implementing-an-http-file-server
 require "socket"
 
-tcpserver = TCPServer.new('localhost', 3000)
+tcpserver = TCPServer.new 3000
 while (socket = tcpserver.accept)
   request = socket.gets
   STDERR.puts request
@@ -15,16 +15,10 @@ while (socket = tcpserver.accept)
   # sensitive, and expects each header line to end with CRLF 
   # (i.e. "\r\n")
   headers = ["HTTP/1.1 200 OK",
-           "Date: Tue, 14 Dec 2010 10:48:45 GMT",
            "Server: Ruby",
-           "Content-Type: text/html; charset=iso-8859-1",
+           "Content-Type: text/plain; charset=iso-8859-1",
            "Content-Length: #{response.length}\r\n\r\n"].join("\r\n")
-
   socket.print headers
-  
-  # Now create a valid HTML page
-  socket.print "<HTML><BODY>"
   socket.print response
-  socket.print "</BODY></HTML>"
   socket.close
 end
